6e229b643a19ac47463c903ae23723e2c95fa07b
Феофилов Максим <e1ektr0@live.ru>
2014-07-24 16:50:30 +0400

    Создал проект. Написал асинхронный скачиватель.

---
+++ StackoverflowAnalizer/Downloader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace StackoverflowAnalizer
+{
+    public class Downloader
+    {
+        private readonly ConcurrentQueue<string> _urls = new ConcurrentQueue<string>();
+        private readonly Dictionary<string, string> _results = new Dictionary<string, string>();
+        private readonly Dictionary<string, Exception> _errors = new Dictionary<string, Exception>();
+        private int _treadCount = 2;
+
+        public async Task AddToQueue(string url)
+        {
+            var startingThread = 0;
+            _urls.Enqueue(url);
+            var neededThread = _urls.Count;
+            while (_treadCount > 0)
+            {
+                _treadCount--;
+                await StartDownloadTread().ContinueWith(n => _treadCount++);
+                startingThread++;
+                if (startingThread == neededThread)
+                    break;
+            }
+        }
+
+        private async Task StartDownloadTread()
+        {
+            string url;
+            if (_urls.TryDequeue(out url))
+            {
+                await await Download(url).ContinueWith(task => StartDownloadTread());
+            }
+        }
+
+        private async Task Download(string url)
+        {
+            try
+            {
+                var result = await new WebClient().DownloadStringTaskAsync(url);
+                _results.Add(url, result);
+            }
+            catch (Exception ex)
+            {
+                _errors.Add(url, ex);
+            }
+        }
+
+        public Dictionary<string, string> GetResult()
+        {
+            return _results.ToDictionary(n => n.Key, n => n.Value);
+        }
+    }
+}
---
+++ StackoverflowAnalizer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackoverflowAnalizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var downloader = new Downloader();
+            var tasks = new []
+            {
+                downloader.AddToQueue("http://stackoverflow.com/questions/tagged/c%23"),
+                downloader.AddToQueue("http://stackoverflow.com/questions/23437760/accessviolationexception-was-unhandled"),    
+                downloader.AddToQueue("http://stackoverflow.com/questions/23205731/entity-framework-6-1-code-first-cascading-delete-with-tph-for-one-to-one-relatio"),
+                downloader.AddToQueue("http://weblogs.asp.net/manavi/inheritance-mapping-strategies-with-entity-framework-code-first-ctp5-part-3-table-per-concrete-type-tpc-and-choosing-strategy-guidelines")
+            };
+            Task.WaitAll(tasks);
+            foreach (var result in downloader.GetResult())
+            {
+                Console.WriteLine(result.Key);
+            }
+        }
+    }
+}
